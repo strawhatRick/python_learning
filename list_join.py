8c26be9726389e58b7d04f907916bd94b7e4fb36
strawhatRick <34947117+strawhatRick@users.noreply.github.com>
2019-04-18 12:16:12 +0530

    this shows the use of join function in list

---
+++ list_join.py
@@ -0,0 +1,14 @@
+musical_groups = [
+    ["Ad Rock", "MCA", "Mike D."],
+    ["John Lennon", "Paul McCartney", "Ringo Starr", "George Harrison"],
+    ["Salt", "Peppa", "Spinderella"],
+    ["Rivers Cuomo", "Patrick Wilson", "Brian Bell", "Scott Shriner"],
+    ["Chuck D.", "Flavor Flav", "Professor Griff", "Khari Winn", "DJ Lord"],
+    ["Axl Rose", "Slash", "Duff McKagan", "Steven Adler"],
+    ["Run", "DMC", "Jam Master Jay"],
+]
+# Your code here
+members = ""
+for group in musical_groups:
+    members = ", ".join(group)
+    print(members)
