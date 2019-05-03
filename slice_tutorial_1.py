fc5ecb3daf0c268da5526ce984f70e0b76170b99
strawhatRick <34947117+strawhatRick@users.noreply.github.com>
2019-05-03 22:28:37 +0530

    basic functions of slice

---
+++ slice_tutorial_1.py
@@ -0,0 +1,8 @@
+favorite_things = ['raindrops on roses', 'whiskers on kittens', 'bright copper kettles',
+                   'warm woolen mittens', 'bright paper packages tied up with string',
+                   'cream colored ponies', 'crisp apple strudels']
+
+slice1 = favorite_things[1:4]
+slice2 = favorite_things[5:]
+sorted_things = favorite_things[:]
+sorted_things.sort()
