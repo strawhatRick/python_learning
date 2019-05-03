9a5fd62e0f5e335f2baa8fb020b078a9a0c5400d
strawhatRick <34947117+strawhatRick@users.noreply.github.com>
2019-05-03 23:55:20 +0530

    implementations of slice

---
+++ numbers_slice_2.py
@@ -0,0 +1,9 @@
+def first_4(l):
+    return l[0:4]
+def first_and_last_4(l):
+    return l[:4] + l[-4:]
+def odds(l):
+    return l[1::2]
+def reverse_evens(l):
+    l1 = l[::2]
+    return l1[::-1]
