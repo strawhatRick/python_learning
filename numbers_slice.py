ed22ff35a09d0bce4efc46d953dcf88f6abbfe9f
strawhatRick <34947117+strawhatRick@users.noreply.github.com>
2019-05-03 22:41:08 +0530

    start, stop, step parts of slice is used here

---
+++ numbers_slice.py
@@ -0,0 +1,9 @@
+numbers = list(range(20))                         
+numbers                                           
+numbers[::2]                                      
+numbers[3::2]                                     
+"Oklahoma"[::2]                                   
+numbers[-2:]                                      
+numbers[-2:-5]                                    
+numbers[-2:-5:-1]                                 
+numbers[::-1]                                     
